e8e2ba6fc7525f68e91d3a1149fccbffd60b4db8
Gelsy Sonnessa <131328732+gelsysonnessa@users.noreply.github.com>
2023-07-11 22:29:40 -0400

    Add files via upload

---
+++ PEP - Ejercicio II - Gelsy Sonnessa.py
@@ -0,0 +1,24 @@
+# EJERCICIO II - PARCIAL 1
+
+H = int(input("Ingrese la profundidad del pozo (en metros): "))
+Ld = int(input("Ingrese lo que asciende el caracol durante el día (en metros): "))
+Ln = int(input("Ingrese lo que desciende el caracol durante la noche (en metros): "))
+
+posicion = Ld - Ln
+L = 0
+dias = 0
+
+if posicion < 0:
+    print("El caracol nunca saldrá del pozo :(")
+    
+while Ld > 0:
+    
+    if posicion > 0:
+        L += posicion
+        dias += 1 # --> 24 horas
+    
+    if L >= H:
+        break
+        
+print("El caracol tardó",dias,"días en salir del pozo") 
+
